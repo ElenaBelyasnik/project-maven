2701072a9759d1253c9d74ebe268328439e1a96c
Elena Belyasnik <elena.belyasnik@yandex.ru>
2022-07-15 21:15:28 +0300

    Create pom.xml.

---
+++ Launcher.bat
@@ -0,0 +1,1 @@
+java -jar "C:\Users\musal\IdeaProjects\project-maven\target\project-maven-1.0.jar"
---
+++ lib/Install_lib.bat
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=C:\Users\musal\IdeaProjects\project-maven\lib\desktop-game-engine.jar -DgroupId=com.javarush \
+    -DartifactId=desktop-game-engine -Dversion=1.0 -Dpackaging=jar
